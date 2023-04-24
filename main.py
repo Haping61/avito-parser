b39c7a040bddbdb3c96420e781fdbb1d675875ca
Haping61 <tg.trippl3a@gmail.com>
2023-04-24 21:41:59 +0700

    fix: rename imported file in main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
-from format_products_to_table import write_data
+from put_products_to_table import write_data
 
 driver = webdriver.Chrome()
 driver.get("https://www.avito.ru")
